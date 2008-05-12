0ac8784d52914e2820e6b6209b71c4ad426d8e51
sholmes.delegata <sholmes.delegata@c7d6fc67-7a4a-0410-be91-4504ca24c4e5>
2008-05-12 18:52:37 +0000

    -fixed some tests

--- trunk/grails-app/conf/BootStrap.groovy
+++ trunk/grails-app/conf/BootStrap.groovy
@@ -35,8 +35,14 @@ class BootStrap {
         new RelationReference(name: "depends on", description: "depends on", order: 1).save()
         new RelationReference(name: "exists in", description: "exists in", order: 2).save()
         new RelationReference(name: "is documented by", description: "is documented by", order: 3).save()
+        new RelationReference(name: "BOM", description: "exclusively owned by", order: 3).save()
+        new RelationReference(name: "is installed on", description: "is installed on", order: 3).save()
+        new RelationReference(name: "is baselined by", description: "is baselined by", order: 3).save()
+        new RelationReference(name: "is certified by", description: "is certified by", order: 3).save()
+        new RelationReference(name: "is prepared by", description: "is prepared by", order: 3).save()
+        new RelationReference(name: "is configured by", description: "is configured by", order: 3).save()
 
-        def planned = new StatusReference(name: "Planned", description: "Planned", order: 1).save()
+        new StatusReference(name: "Planned", description: "Planned", order: 1).save()
         new StatusReference(name: "Implemented", description: "Implemented", order: 2).save()
         def active = new StatusReference(name: "Active", description: "Active", order: 3).save()
         new StatusReference(name: "Inactive", description: "Inactive", order: 4).save()
@@ -47,79 +53,84 @@ class BootStrap {
 
         def testing = new Environment(name: "Testing", project: aps).save()
 
-        def server = new HardwareType(description:'Server', order:1).save(flush:true)
-        def solutions = new Hardware(
+
+        def server = new HardwareType(description: 'Server', order: 1).save(flush: true)
+        def os = new SoftwareType(description: 'Operating System', order: 1).save(flush: true)
+        new SoftwareType(description: 'Patch Level', order: 1).save(flush: true)
+        new SoftwareType(description: 'Database Server', order: 1).save(flush: true)
+        new SoftwareType(description: 'Application Server', order: 1).save(flush: true)
+        new SoftwareType(description: 'Web Server', order: 1).save(flush: true)
+        new SoftwareType(description: 'Application', order: 1).save(flush: true)
+        new SoftwareType(description: 'Database Schema', order: 1).save(flush: true)
+        new SoftwareType(description: 'Script', order: 1).save(flush: true)
+        def networkType = new NetworkType(description: 'Network', order: 1).save(flush: true)
+        def documentationType = new DocumentationType(description: 'asdfasdf', order: 1).save(flush: true)
+        new RequestType(description: 'Requirements', order: 1).save(flush: true)
+        new TestResultType(description: 'Unit Tests', order: 1).save(flush: true)
+
+        new Hardware(
                 name: "Solutions",
                 author: "Steve Holmes",
                 purchaseDate: new Date(),
-                hardwareType: server).save(flush:true)
-        def isis = new Hardware(
+                hardwareType: server).save(flush: true)
+        new Hardware(
                 name: "Isis",
                 author: "Steve Holmes",
                 purchaseDate: new Date(),
-                hardwareType: server).save(flush:true)
+                hardwareType: server).save(flush: true)
 
         def startDate = new Date() - 10
-        (1..10).each { element ->
+        (1..10).each {element ->
             new Status(
-                startDate: startDate,
-                endDate: startDate + 10000,
-                configurationItem: new Hardware(
-                    name: "hardware${element}",
-                    author: "Steve Holmes",
-                    purchaseDate:new Date(),
-                    hardwareType:server).save(flush:true),
-                reference: active).save(flush:true)
+                    startDate: startDate,
+                    endDate: startDate + 10000,
+                    configurationItem: new Hardware(
+                            name: "hardware${element}",
+                            author: "Steve Holmes",
+                            purchaseDate: new Date(),
+                            hardwareType: server).save(flush: true),
+                    reference: active).save(flush: true)
         }
 
         testing.configurationItems = Hardware.getAll()
-        testing.save(flush:true)
+        testing.save(flush: true)
         def hardwares = Hardware.getAll()
-        hardwares.each{ hardware ->
-            (11..15).each{ element ->
+        hardwares.each {hardware ->
+            (11..15).each {element ->
                 new Status(
-                startDate: startDate,
-                endDate: startDate + 10000,
-                configurationItem: new Hardware(
-                    name: "hardware ${element} ${hardware.id}",
-                    author: "Steve Holmes",
-                    purchaseDate:new Date(),
-                    hardwareType:server,
-                    parent: hardware).save(flush:true),
-                reference: active).save(flush:true)
+                        startDate: startDate,
+                        endDate: startDate + 10000,
+                        configurationItem: new Hardware(
+                                name: "hardware ${element} ${hardware.id}",
+                                author: "Steve Holmes",
+                                purchaseDate: new Date(),
+                                hardwareType: server,
+                                parent: hardware).save(flush: true),
+                        reference: active).save(flush: true)
             }
         }
-
-
-
-        def os = new SoftwareType(description:'Operating System', order:1).save(flush:true)
 
         def windows = new Software(
                 name: "Windows XP Professional",
                 author: "Steve Holmes",
                 softwareType: os)
-        if(!windows.save(flush:true)){
-            windows.errors.each{error -> println error}
+        if (!windows.save(flush: true)) {
+            windows.errors.each {error -> println error}
         }
 
-        def networkType = new NetworkType(description:'Network', order:1).save(flush:true)
-
-        new Network(name:'network 1', author:"Steve Holmes", networkType:networkType).save(flush:true)
-
-        def documentationType = new DocumentationType(description:'asdfasdf', order:1).save(flush:true)
+        new Network(name: 'network 1', author: "Steve Holmes", networkType: networkType).save(flush: true)
 
         new Documentation(
-                name:'Document 1',
-                author:'Steve Holmes',
-                version:1,
-                fileType:'what',
-                fileName:'ever',
-                documentationType:documentationType).save(flush:true)
+                name: 'Document 1',
+                author: 'Steve Holmes',
+                version: 1,
+                fileType: 'what',
+                fileName: 'ever',
+                documentationType: documentationType).save(flush: true)
 
-        def requestType = new RequestType(description:'asdfasdf', order:1).save(flush:true)
-        def testResultType = new TestResultType(description:'asdfasdf', order:1).save(flush:true)
 
     }
+
     def destroy = {
     }
 } 
