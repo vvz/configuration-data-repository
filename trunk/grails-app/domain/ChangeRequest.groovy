785e203e66c8b38609a926c8b3c611b60d8bf1bc
sholmes.delegata <sholmes.delegata@c7d6fc67-7a4a-0410-be91-4504ca24c4e5>
2008-05-21 23:03:54 +0000

    -Made additional changes to ChangeRequest and TestResult to handle documents in Oracle -added CDRTagLib -Added BlobUtil, utility class for byte[] to BLOB conversions

--- trunk/grails-app/domain/ChangeRequest.groovy
+++ trunk/grails-app/domain/ChangeRequest.groovy
@@ -1,6 +1,11 @@
+import com.delegata.utility.BlobUtil
+import org.hibernate.Hibernate
+import java.sql.Blob
+
 class ChangeRequest extends ConfigurationItem{
 
     byte[] document
+    Blob documentBlob
     String fileType
     String fileName
     String fileSize
@@ -9,13 +14,39 @@ class ChangeRequest extends ConfigurationItem{
     Date dateCreated
     Date lastUpdated
 
+    static transients = ["document"]
     static belongsTo = RequestType
     static constraints = {
         document(nullable:true)
+        documentBlob(nullable: true)
         fileType(nullable:true)
         fileName(nullable:true)
         fileSize(nullable:true)
         requestType(nullable:false, blank:false)
+    }
+
+    static mapping = {
+        documentBlob type: 'blob'
+    }
+
+    def getDocument() {
+        if (documentBlob == null)
+            return null;
+        return BlobUtil.toByteArray(getDocumentBlob());
+    }
+
+    def setDocument(document) {
+        setDocumentBlob(Hibernate.createBlob(document));
+    }
+
+    boolean equals(obj) {
+        if (this == obj) return true;
+        if (!obj || obj.class != this.class) return false;
+        return id?.equals(obj.id)
+    }
+
+    int hashCode() {
+        return id ? id.hashCode() : super.hashCode()
     }
 
     String toString(){
