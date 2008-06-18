9c7850db17c6e5ff480fa0e04f92f583f25e3aec
sholmes.delegata <sholmes.delegata@c7d6fc67-7a4a-0410-be91-4504ca24c4e5>
2008-06-18 21:57:39 +0000

    -New Datasource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,49 +1,29 @@
-dataSource {
-    pooled = false
-    driverClassName = "org.hsqldb.jdbcDriver"
-    username = "sa"
-    password = ""
-}
 hibernate {
-    cache.use_second_level_cache=true
-    cache.use_query_cache=true
-    cache.provider_class='org.hibernate.cache.EhCacheProvider'
+    cache.use_second_level_cache = true
+    cache.use_query_cache = true
+    cache.provider_class = 'org.hibernate.cache.EhCacheProvider'
 }
 // environment specific settings
 environments {
     development {
         dataSource {
+            pooled = false
+            driverClassName = "org.hsqldb.jdbcDriver"
+            username = "sa"
+            password = ""
             dbCreate = "create-drop" // one of 'create', 'create-drop','update'
             url = "jdbc:hsqldb:mem:devDB"
         }
     }
     test {
         dataSource {
-            dbCreate = "update"
-            driverClassName = "oracle.jdbc.OracleDriver"
-            username = "cdrtest"
-            password = "cdrtest"
-            url = "jdbc:oracle:thin:@//192.168.0.235:1521/COAPS"
-            
-        }
-    }
-    production {
-        dataSource {
-            dbCreate = "update"
-            driverClassName = "oracle.jdbc.OracleDriver"
-            username = "cdrtrain"
-            password = "cdrtrain"
-            url = "jdbc:oracle:thin:@192.168.0.235:1521:COAPS"
-        }
-    }
-
-    /*production {
-        dataSource {
+            pooled = false
             dbCreate = "create-drop"
             driverClassName = "oracle.jdbc.OracleDriver"
             username = "cdrint"
             password = "cdrint"
             url = "jdbc:oracle:thin:@192.168.0.235:1521:COAPS"
+
         }
     }
 
@@ -51,9 +31,7 @@ environments {
         dataSource {
             dbCreate = "update"
             driverClassName = "oracle.jdbc.OracleDriver"
-            username = "cdrprod"
-            password = "cdrprod"
-            url = "jdbc:oracle:thin:@192.168.0.235:1521:COAPS"
+            jndiName = "java:comp/env/jdbc/cdr"
         }
-    }*/
+    }
 }
