42eef098b85089fcf80e8d13e108bbf7797ab1bc
sholmes.delegata <sholmes.delegata@c7d6fc67-7a4a-0410-be91-4504ca24c4e5>
2008-07-11 18:08:01 +0000

    -Added commented out lines to the CRCT for training purposes

--- branches/HDC_Trunk/CDR/test/integration/ChangeRequestControllerTests.groovy
+++ branches/HDC_Trunk/CDR/test/integration/ChangeRequestControllerTests.groovy
@@ -1,7 +1,10 @@
-class ChangeRequestControllerTests extends GroovyTestCase{
-  public void testIndex() {
+class ChangeRequestControllerTests extends GroovyTestCase {
+    //class ChangeRequestControllerTests extendzzz GroovyTestCase {   //compile error
+    public void testIndex() {
         def controller = new ChangeRequestController()
         controller.index()
         assertEquals "/changeRequest/list", controller.response.redirectedUrl
+        //assertEquals "this", "that" //test failure
+        //assertEquals "this", "this" //legitimate change to test, should work
     }
 }
