b5a7ea8e2ae761dd892badb274118a7c6d16367b
delegatauser1@gmail.com <delegatauser1@gmail.com@c7d6fc67-7a4a-0410-be91-4504ca24c4e5>
2008-07-11 22:21:58 +0000

    change

--- branches/HDC_Trunk/CDR/test/integration/ChangeRequestControllerTests.groovy
+++ branches/HDC_Trunk/CDR/test/integration/ChangeRequestControllerTests.groovy
@@ -7,5 +7,6 @@ class ChangeRequestControllerTests extends GroovyTestCase {
         //assertEquals "this", "that" //test failure
         //assertEquals "this", "this" //legitimate change to test, should work
         println "Make changes to this line so that it causes a conflict in subversion with another users changes to this line"
+
     }
 }
