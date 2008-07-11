bb4bf89bf585adc58e5a2f77b320c690ea11947d
iamsteveholmes <iamsteveholmes@c7d6fc67-7a4a-0410-be91-4504ca24c4e5>
2008-07-11 23:52:14 +0000

    Tagged from HDC_CDR_Production #1

--- CDR/test/integration/ChangeRequestControllerTests.groovy
+++
@@ -1,7 +0,0 @@
-class ChangeRequestControllerTests extends GroovyTestCase{
-  public void testIndex() {
-        def controller = new ChangeRequestController()
-        controller.index()
-        assertEquals "/changeRequest/list", controller.response.redirectedUrl
-    }
-}
