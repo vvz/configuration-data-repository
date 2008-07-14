ac7e2c77a7c72bd51bf4197a8cbc0b89c2ec1730
delegatauser2 <delegatauser2@c7d6fc67-7a4a-0410-be91-4504ca24c4e5>
2008-07-14 22:43:23 +0000

    Remove line causing test failure

--- branches/red/CDR/test/integration/ChangeRequestControllerTests.groovy
+++ branches/red/CDR/test/integration/ChangeRequestControllerTests.groovy
@@ -4,7 +4,7 @@ class ChangeRequestControllerTests extends GroovyTestCase {
         def controller = new ChangeRequestController()
         controller.index()
         assertEquals "/changeRequest/list", controller.response.redirectedUrl
-        assertEquals "this", "that" //test failure
+        //assertEquals "this", "that" //test failure
         assertEquals "this", "this" //legitimate change to test, should work
         println "Make changes to this line so that it causes a conflict in subversion with another users changes to this line"
 
