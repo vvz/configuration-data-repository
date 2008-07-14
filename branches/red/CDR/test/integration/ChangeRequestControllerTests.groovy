e3529c38cb8a3f7ef8e794ed7172c35003754d9d
delegatauser2 <delegatauser2@c7d6fc67-7a4a-0410-be91-4504ca24c4e5>
2008-07-14 21:04:20 +0000

    Uncommented assert

--- branches/red/CDR/test/integration/ChangeRequestControllerTests.groovy
+++ branches/red/CDR/test/integration/ChangeRequestControllerTests.groovy
@@ -5,7 +5,7 @@ class ChangeRequestControllerTests extends GroovyTestCase {
         controller.index()
         assertEquals "/changeRequest/list", controller.response.redirectedUrl
         //assertEquals "this", "that" //test failure
-        //assertEquals "this", "this" //legitimate change to test, should work
+        assertEquals "this", "this" //legitimate change to test, should work
         println "Make changes to this line so that it causes a conflict in subversion with another users changes to this line"
 
     }
