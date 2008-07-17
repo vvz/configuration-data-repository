62b5a85377c747063bc2e369e3e744d2162a048d
delegatauser7 <delegatauser7@c7d6fc67-7a4a-0410-be91-4504ca24c4e5>
2008-07-17 21:00:50 +0000

    corrected conflicts

--- branches/green/CDR/test/integration/ChangeRequestControllerTests.groovy
+++ branches/green/CDR/test/integration/ChangeRequestControllerTests.groovy
@@ -6,7 +6,7 @@ class ChangeRequestControllerTests extends GroovyTestCase {
         assertEquals "/changeRequest/list", controller.response.redirectedUrl
         //assertEquals "this", "that" //test failure
         //assertEquals "this", "this" //legitimate change to test, should work
+        println "David Matlock"
         println "Hello Steve!!!!Make changes to this line so that it causes a conflict in subversion with another users changes to this line"
-
     }
 }
