830b83c00e54d71761cec813d32bbc551e0e1882
delegatauser1 <delegatauser1@c7d6fc67-7a4a-0410-be91-4504ca24c4e5>
2008-07-17 22:25:42 +0000

    - adding requirements and test results

--- branches/purple/CDR/CDRClient_scripts/LoadRequirements.groovy
+++ branches/purple/CDR/CDRClient_scripts/LoadRequirements.groovy
@@ -21,9 +21,9 @@ def buildCiType = "Application"
 def buildCiStatusName = "Active"
 
 def requirementCiName = "Requirements Document"
-def requirementCiAuthor = "Steve Holmes"
+def requirementCiAuthor = "Joy Hennessy"
 def requirementCiType = "Requirements"
-def requirementFileAddress = "/usr/local/hudson_home/jobs/HDC_CDR_Integration/workspace/CDR/CDRClient_scripts/Requirements.csv"
+def requirementFileAddress = "/usr/local/hudson_home/jobs/HDC_CDR_Staging_purple/workspace/CDR/CDRClient_scripts/Requirements.csv"
 def requirementStatus = "Active"
 
 def relationType = "baselines"
