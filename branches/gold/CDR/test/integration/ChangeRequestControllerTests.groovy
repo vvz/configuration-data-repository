dce223d9f2d4558e773ea6b1f27250f64a7ae544
delegatauser2 <delegatauser2@c7d6fc67-7a4a-0410-be91-4504ca24c4e5>
2008-07-17 20:44:16 +0000

    Eamonn made this change

--- branches/gold/CDR/test/integration/ChangeRequestControllerTests.groovy
+++ branches/gold/CDR/test/integration/ChangeRequestControllerTests.groovy
@@ -6,7 +6,7 @@ class ChangeRequestControllerTests extends GroovyTestCase {
         assertEquals "/changeRequest/list", controller.response.redirectedUrl
         //assertEquals "this", "that" //test failure
         //assertEquals "this", "this" //legitimate change to test, should work
-        println "Make changes to this line so that it causes a conflict in subversion with another users changes to this line"
+        println " println "Eamonn made this change"
 
     }
 }
